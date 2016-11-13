dbc02dabe1813324c06fcf10602c954abb26c409
asildnes <andsild@gmail.com>
2016-11-13 18:20:41 +0100

    added to travis

--- installSubmodules.sh
+++ installSubmodules.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+#@ DESCRIPTION: print error msg and exit with a given ret value
+die() #@ USAGE: die STAUTS [MESSAGE]
+{
+    error=$1
+    shift
+    [ -n "$*" ] && printf "%s\n" "$*" >&2
+    exit "$error"
+}
+
 subreponame="TextToNumber"
 
 if [ ! -d "./${subreponame}" ]
@@ -16,16 +25,3 @@ then
 fi
 
 test -e ./${subreponame}/dist/build/${subreponame}/${subreponame}.exe || die "Setup did not work. Try cloning submodule manually and run cabal install (see source code for script)"
-
-pip install -r requirements.txt
-
-
-
-#@ DESCRIPTION: print error msg and exit with a given ret value
-die() #@ USAGE: die STAUTS [MESSAGE]
-{
-    error=$1
-    shift
-    [ -n "$*" ] && printf "%s\n" "$*" >&2
-    exit "$error"
-}
