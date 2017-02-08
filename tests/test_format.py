d64399472e9fb0d4ba32eddd57d263caef387b83
asildnes <andsild@gmail.com>
2017-02-08 18:48:29 +0100

    rewrote some messy tests

--- tests/test_format.py
+++ tests/test_format.py
@@ -10,20 +10,33 @@ class FormatTest(unittest2.TestCase):
     def tearDown(self):
         pass
 
-    def test_spoken_forms(self):
-        testInOut("hello world", "hello world", [FormatTypes.spokenForm])
-        testInOut("camel case", "camelCase", [FormatTypes.camelCase])
-        testInOut("snake case", "snake_case", [FormatTypes.snakeCase])
-        testInOut("pascal case", "PascalCase", [FormatTypes.pascalCase])
-        testInOut("LOWERCASE WORD", "lowercase word", [FormatTypes.lowerCase])
-        testInOut("uppercase word", "UPPERCASE WORD", [FormatTypes.upperCase])
-        testInOut("squash case", "squashcase", [FormatTypes.squash])
+    def test_spoken_form(self):
+        res = format_text_logic("hello world", [FormatTypes.spokenForm])
+        res.should.be.equal("hello world")
 
+    def test_camel_form(self):
+        res = format_text_logic("camel case", [FormatTypes.camelCase])
+        res.should.be.equal("camelCase")
 
-def testInOut(input, output, formatTypes):
-    res = format_text_logic(input, formatTypes)
-    res.should.be.equal(output)
+    def test_snake_form(self):
+        res = format_text_logic("snake case", [FormatTypes.snakeCase])
+        res.should.be.equal("snake_case")
 
+    def test_pascal_form(self):
+        res = format_text_logic("pascal case", [FormatTypes.pascalCase])
+        res.should.be.equal("PascalCase")
+
+    def test_lowercase_form(self):
+        res = format_text_logic("LOWERCASE WORD", [FormatTypes.lowerCase])
+        res.should.be.equal("lowercase word")
+
+    def test_uppercase_form(self):
+        res = format_text_logic("uppercase word", [FormatTypes.upperCase])
+        res.should.be.equal("UPPERCASE WORD")
+
+    def test_squash_form(self):
+        res = format_text_logic("squash case",[FormatTypes.squash])
+        res.should.be.equal("squashcase")
 
 if __name__ == "__main__":
     unittest2.main()
